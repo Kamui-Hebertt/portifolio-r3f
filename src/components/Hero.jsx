72aebf7256eab58e0b8f0bef3c734dd0c7612feb
kamui-Hebertt <heberttaferreira.com@gmail.com>
2023-08-08 17:57:09 -0300

    feat: hero: animations, width font styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,17 +15,59 @@ const Hero = () => {
         bounce: 0.5,
         satisfies:100,
         duration: 1,
-        delay: 1,
+        delay: 2,
       },
 
     },
     hidden : { opacity:0, scale:0.5 }
   }
+
+
+
+  const h2variants = {
+    visible: {
+      opacity: 1,
+      scale:1.5,
+      transition: {
+        type:'spring',
+        bounce: 0.5,
+        satisfies:100,
+        duration: 0.8,
+        delay: 2.5,
+      },
+
+    },
+    hidden : { opacity:0, scale:0.5 }
+  }
+
+
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(()=> {
+    if(inView) {
+      controls.start('visible');
+
+    }
+  },[controls, inView])
   
   return(
     <section className="w-screen h-screen mx-auto">
       <div className="absolute left-[5%] top-[5%] xl:top-[14%] mx-auto fredoka-font text-white">
-      <motion.h1 variants={h1variants} className="text-5xl lg:text-7xl">Hi, i'm <span className="hover-text-glow light-blue-text font-semibold">Hebertt</span></motion.h1>
+      <motion.h1
+       ref={ref}
+       animate={controls}
+       initial='hidden'
+       variants={h1variants}
+        className="text-5xl lg:text-7xl">Hi, i'm <span className="hover-text-glow light-blue-text font-semibold">Hebertt</span>
+        </motion.h1>
+        <motion.h2
+       ref={ref}
+       animate={controls}
+       initial='hidden'
+       variants={h2variants}
+        className="mt-8 text-3x1 w-3/3 text-center">i'm a full stack web developer 
+        </motion.h2>
         </div>  
 
     </section>

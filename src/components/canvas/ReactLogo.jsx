34fe1583fd74e8170a2cb05e6de7ed4f659a7878
kamui-Hebertt <heberttaferreira.com@gmail.com>
2023-08-08 00:04:46 -0300

    feat: animated logo

--- src/components/canvas/ReactLogo.jsx
+++ src/components/canvas/ReactLogo.jsx
@@ -1,9 +1,31 @@
-import { useGLTF } from "@react-three/drei"
-import { useMemo } from "react";
+import { useAnimations, useGLTF } from "@react-three/drei"
+
+import { useEffect, useMemo } from "react";
+
+
+const actionNames = [
+  'SphereAction',
+  'TorusAction1',
+  'TorusAction2',
+  'TorusAction3',
+]
 
 const ReactLogo = () => {
   const reactLogo = useMemo(()=> useGLTF('./reactLogo/scene.gltf'), []);
    
+
+  // animation
+  const animations = useAnimations(reactLogo.animations, reactLogo.scene);
+
+
+  useEffect(()=> {
+    actionNames.forEach((actionsName) => {
+      const action = animations.actions[actionsName];
+      action.play();
+    })
+  },[animations]);
+
+
   return (
     <mesh>
       <primitive
